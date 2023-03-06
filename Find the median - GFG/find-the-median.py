b09003f0e944f45f92992dc8bf1db48482dcc343
Saquib Hussain <109733382+Hussainaquib@users.noreply.github.com>
2023-03-06 16:53:40 +0530

    Added solution - LeetHub

---
+++ Find the median - GFG/find-the-median.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+import statistics
+class Solution:
+	def find_median(self, v):
+	    return int(statistics.median(v))
+		   
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		v = list(map(int,input().split())) 
+		ob = Solution();
+		ans = ob.find_median(v)
+		print(ans)
+# } Driver Code Ends
