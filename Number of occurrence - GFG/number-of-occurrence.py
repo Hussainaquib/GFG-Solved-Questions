03c0d86f0eb3c51e987439f72abc1fba28b06e21
Saquib Hussain <109733382+Hussainaquib@users.noreply.github.com>
2023-06-10 13:46:51 +0530

    Added solution - LeetHub

---
+++ Number of occurrence - GFG/number-of-occurrence.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+class Solution:
+
+	def count(self,arr, n, x):
+		c=0
+		for i in range(0,len(arr)):
+		    if arr[i]==x:
+		        c+=1
+		return c
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+
+if __name__ == '__main__':
+    tc = int(input())
+    while tc > 0:
+        n, x = list(map(int, input().strip().split()))
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        ans = ob.count(arr, n, x)
+        print(ans)
+        tc -= 1
+
+# } Driver Code Ends
